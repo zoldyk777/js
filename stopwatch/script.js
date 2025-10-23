a3aa572938947724ccdd500529e37810fb7eb548
zoldyk777 <adityabisht630@gmail.com>
2025-10-23 22:39:56 +0530

    style

---
+++ stopwatch/script.js
@@ -0,0 +1,56 @@
+const display = document.getElementById("display");
+let timer = null;
+let starttime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+    if (!isRunning) {
+        starttime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+
+
+}
+
+function stop() {
+
+    if (isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - starttime;
+        isRunning = false;
+    }
+
+
+}
+
+function reset() {
+
+    clearInterval(timer);
+    starttime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+
+}
+
+function update() {
+    const currenttime = Date.now();
+    elapsedTime = currenttime - starttime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+
+
+
+}
